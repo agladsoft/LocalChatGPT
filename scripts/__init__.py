c9aa1a1383c3013e5e7297c377e9d4be63dfaa15
timur <timurka.zavyalov@mail.ru>
2023-12-30 23:48:01 +0300

    feat: add remove documents in ui

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -88,4 +88,21 @@ tr span {
   user-select: all; /* Разрешаем выделение текста */
 }
 
+
+
+/* Works on Chrome, Edge, and Safari */
+*::-webkit-scrollbar {
+  width: 12px;
+}
+
+*::-webkit-scrollbar-track {
+  background: orange;
+}
+
+*::-webkit-scrollbar-thumb {
+  background-color: blue;
+  border-radius: 20px;
+  border: 3px solid orange;
+}
+
 """
