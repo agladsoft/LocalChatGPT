a8993a219e848a2faf62a1320e5842af4b3b3c1d
timur <timurka.zavyalov@mail.ru>
2024-01-22 16:37:58 +0300

    feat: add remove documents in ui

--- scripts/app.py
+++ scripts/app.py
@@ -1,8 +1,10 @@
 import re
 import csv
+import time
 import os.path
 import chromadb
 import tempfile
+import threading
 import pandas as pd
 import gradio as gr
 from re import Pattern
@@ -19,6 +21,7 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 class LocalChatGPT:
     def __init__(self):
+        self.semaphore = threading.Semaphore()
         self.llama_model: Optional[Llama] = None
         self.llama_models, self.embeddings = self.initialize_app()
         self.collection: str = "all-documents"
@@ -202,11 +205,14 @@ class LocalChatGPT:
         else:
             return gr.update(placeholder=self.system_prompt, interactive=False)
 
-    @staticmethod
-    def user(message, history):
+    def user(self, message, history):
+        print("Обработка вопроса")
+        self.semaphore.acquire()
         if history is None:
             history = []
         new_history = history + [[message, None]]
+        self.semaphore.release()
+        print("Закончена обработка вопроса")
         return "", new_history
 
     @staticmethod
@@ -256,54 +262,61 @@ class LocalChatGPT:
         :param model_selector:
         :return:
         """
-        if not history or not history[-1][0]:
-            yield history[:-1]
-            return
-        model = next((model for model in self.llama_models if model_selector in model.model_path), None)
-        tokens = self.get_system_tokens(model)[:]
-        tokens.append(LINEBREAK_TOKEN)
+        print("Получили контекст. Начинается подготовка к генерации ответа")
+        while not self.semaphore.acquire(blocking=False):
+            print("Пока еще генерация ответа недоступна")
+            time.sleep(1)
+        else:
+            print("Начинается генерация ответа")
+            if not history or not history[-1][0]:
+                yield history[:-1]
+                return
+            model = next((model for model in self.llama_models if model_selector in model.model_path), None)
+            tokens = self.get_system_tokens(model)[:]
+            tokens.append(LINEBREAK_TOKEN)
 
-        for user_message, bot_message in history[-4:-1]:
-            message_tokens = self.get_message_tokens(model=model, role="user", content=user_message)
+            for user_message, bot_message in history[-4:-1]:
+                message_tokens = self.get_message_tokens(model=model, role="user", content=user_message)
+                tokens.extend(message_tokens)
+
+            last_user_message = history[-1][0]
+            pattern = r'<a\s+[^>]*>(.*?)</a>'
+            files = re.findall(pattern, retrieved_docs)
+            for file in files:
+                retrieved_docs = re.sub(fr'<a\s+[^>]*>{file}</a>', file, retrieved_docs)
+            if retrieved_docs and collection_radio == MODES[0]:
+                last_user_message = f"Контекст: {retrieved_docs}\n\nИспользуя только контекст, ответь на вопрос: " \
+                                        f"{last_user_message}"
+            message_tokens = self.get_message_tokens(model=model, role="user", content=last_user_message)
             tokens.extend(message_tokens)
 
-        last_user_message = history[-1][0]
-        pattern = r'<a\s+[^>]*>(.*?)</a>'
-        files = re.findall(pattern, retrieved_docs)
-        for file in files:
-            retrieved_docs = re.sub(fr'<a\s+[^>]*>{file}</a>', file, retrieved_docs)
-        if retrieved_docs and collection_radio == MODES[0]:
-            last_user_message = f"Контекст: {retrieved_docs}\n\nИспользуя только контекст, ответь на вопрос: " \
-                                    f"{last_user_message}"
-        message_tokens = self.get_message_tokens(model=model, role="user", content=last_user_message)
-        tokens.extend(message_tokens)
-
-        role_tokens = [model.token_bos(), BOT_TOKEN, LINEBREAK_TOKEN]
-        tokens.extend(role_tokens)
-        generator = model.generate(
-            tokens,
-            top_k=top_k,
-            top_p=top_p,
-            temp=temp
-        )
-
-        partial_text = ""
-        for i, token in enumerate(generator):
-            if token == model.token_eos() or (MAX_NEW_TOKENS is not None and i >= MAX_NEW_TOKENS):
-                break
-            partial_text += model.detokenize([token]).decode("utf-8", "ignore")
-            history[-1][1] = partial_text
-            yield history
-
-        if files:
-            partial_text += SOURCES_SEPARATOR
-            sources_text = "\n\n\n".join(
-                f"{index}. {source}"
-                for index, source in enumerate(files, start=1)
+            role_tokens = [model.token_bos(), BOT_TOKEN, LINEBREAK_TOKEN]
+            tokens.extend(role_tokens)
+            generator = model.generate(
+                tokens,
+                top_k=top_k,
+                top_p=top_p,
+                temp=temp
             )
-            partial_text += sources_text
-            history[-1][1] = partial_text
-        yield history
+
+            partial_text = ""
+            for i, token in enumerate(generator):
+                if token == model.token_eos() or (MAX_NEW_TOKENS is not None and i >= MAX_NEW_TOKENS):
+                    break
+                partial_text += model.detokenize([token]).decode("utf-8", "ignore")
+                history[-1][1] = partial_text
+                yield history
+
+            if files:
+                partial_text += SOURCES_SEPARATOR
+                sources_text = "\n\n\n".join(
+                    f"{index}. {source}"
+                    for index, source in enumerate(files, start=1)
+                )
+                partial_text += sources_text
+                history[-1][1] = partial_text
+            yield history
+        self.semaphore.release()
 
     def ingest_files(self):
         db = self.load_db()
