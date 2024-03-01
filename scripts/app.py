8dc61b51bc9e0fa872d1d83bea143e5b60a2a84b
timur <timurka.zavyalov@mail.ru>
2024-03-01 09:34:33 +0300

    feat: add changing model in load documents

--- scripts/app.py
+++ scripts/app.py
@@ -72,12 +72,13 @@ class LocalChatGPT:
         :return:
         """
         if is_load_model:
-            time.sleep(15)
+            time.sleep(20)
             del self.embeddings
             del self.db
 
             self.embeddings = HuggingFaceEmbeddings(model_name=EMBEDDER_NAME, cache_folder=MODELS_DIR)
             self.load_db()
+
             self.llama_model = self.initialize_app()
             gr.Info("Модель загружена, можете задавать вопросы")
         else:
